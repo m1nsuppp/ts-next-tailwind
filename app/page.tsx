13fea15faf3a1b2e8cf1dc08e12f9b7bde49d06b
m1nsuppp <m1nsuppp@gmail.com>
2022-11-28 00:36:48 +0900

    boilerplate

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import "styles/globals.css";
+
+export default function IndexPage() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <h1>Hello, Next.js & TS & tailwindcss boilerplate.</h1>
+    </div>
+  );
+}
